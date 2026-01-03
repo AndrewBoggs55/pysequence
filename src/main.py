008f0984d8be97188b7f5a44067a17119a5df91f
Andrew Boggs <166353465+AndrewBoggs55@users.noreply.github.com>
2026-01-03 13:33:40 -0600

    Updated to use a function and parse arg passed from cli

--- src/main.py
+++ src/main.py
@@ -1,18 +1,24 @@
+import argparse
 import wavedrom
 
-svg = wavedrom.render("""  # Creates a class of wavedrom render
-    {
-    "config" : {
-        "bgcolor": "white"
-    },
-    "signal": [
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--out", required=True)
+    args = parser.parse_args()
+
+    svg = wavedrom.render(r"""
+    {"signal": [
         {"name" : "Power 1", "wave" : "l..h..."},
         {"name" : "Power 2", "wave" : "l...h.."},
         {"name" : "Power 3", "wave" : "l....h."},
         {"name" : "Power 4", "wave" : "l....h."},
         {"name" : "Power 5", "wave" : "l.....h"}
-    ]
-    }
-""")
+    ]}
+    """)
 
-svg.saveas("power_sequence.svg")
+    svg.saveas(args.out)
+
+
+if __name__ == "__main__":
+    main()
