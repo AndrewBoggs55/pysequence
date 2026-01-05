669f6b4c4f813ac200527a66bbd1008b55990fc1
Andrew Boggs <166353465+AndrewBoggs55@users.noreply.github.com>
2026-01-04 19:25:35 -0600

    Title, subtext, enable triggers, et.c

--- src/main.py
+++ src/main.py
@@ -9,12 +9,33 @@ def main():
 
     svg = wavedrom.render(r"""
     {"signal": [
-        {"name" : "Power 1", "wave" : "l..h..."},
-        {"name" : "Power 2", "wave" : "l...h.."},
-        {"name" : "Power 3", "wave" : "l....h."},
-        {"name" : "Power 4", "wave" : "l....h."},
+        ["Default",
+            {"name" : "Power 1", "wave" : "l..h..."}
+        ],
+        ["PG_P12V_BUSBAR_EF",
+            {"name" : "Power 2", "wave" : "l...h.."}
+        ],
+        ["POR_VPD_RESET (12 sec delay)",
+            {"name" : "Power 3", "wave" : "l....h.", "node" : .....3."},
+            {},
+            {"name" : "P12V_SYS_SW_[0:1]", "wave" : "l....h."}
+        ],
+        {"name" : "Power 4", "wave" : "l.....h", "node" : "......4"},
         {"name" : "Power 5", "wave" : "l.....h"}
-    ]}
+    ],
+    "edge" : [
+        '3~>4 12s delay'
+    ],
+    "head" : {
+        "text" :
+        ["tspan",
+            {"class" : "success h4"}, "SC2 Power Sequence"]
+    },
+    "foot" : {
+        "text" : "Figure 1",
+        "tock" : 1
+    },
+    }
     """)
 
     svg.saveas(args.out)
